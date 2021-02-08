bdcb1a1fcf4aae324c92c087bc845dcccf66efb6
EvanG <evan_g@orange.fr>
2021-02-08 21:49:08 +0100

    Initial commit

---
+++ SpecularMerMaps/Data.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace JavaBedrockUtilities
+{
+    public enum Type
+    {
+        mer, // MER
+        s // Specular
+    }
+    public class Data
+    {
+        public byte Metalness;
+        public byte Emissive;
+        public byte Roughness;
+
+        public static Data Parse(Color c, Type type)
+        {
+            if (type == Type.mer) return ParseMER(c);
+            else if (type == Type.s) return ParseSpecular(c);
+            else return new Data();
+        }
+        public static Color ToColor(Data d, Type type)
+        {
+            if (type == Type.mer) return ToMER(d);
+            else if (type == Type.s) return ToSpecular(d);
+            else return Color.Transparent;
+        }
+
+        public static Data ParseMER(Color c) => new Data { Metalness = c.R, Emissive = c.G, Roughness = c.B };
+        public static Color ToMER(Data d) => Color.FromArgb(d.Metalness, d.Emissive, d.Roughness);
+
+        public static Data ParseSpecular(Color c) => new Data { Roughness = (byte)(255 - c.R), Metalness = c.G, Emissive = c.B };
+        public static Color ToSpecular(Data d) => Color.FromArgb((byte)(255 - d.Roughness), d.Metalness, d.Emissive);
+    }
+}
---
+++ SpecularMerMaps/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace JavaBedrockUtilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            if (args.Contains("-input")) input = args[Array.IndexOf(args, "-input") + 1];
+            else
+            {
+                Console.Write("Input dir: ");
+                input = Console.ReadLine();
+            }
+
+            string outputDir = "";
+            if (args.Contains("-output")) outputDir = args[Array.IndexOf(args, "-output") + 1];
+            else
+            {
+                Console.Write("Output dir (default: out): ");
+                outputDir = Console.ReadLine();
+            }
+            outputDir = Path.GetFullPath(string.IsNullOrWhiteSpace(outputDir) ? "out" : outputDir).TrimEnd('\\', '/') + "/";
+            if (!Directory.Exists(outputDir)) Directory.CreateDirectory(outputDir);
+
+            Type outputType = Type.mer;
+            if (args.Contains("-type")) outputType = (Type)Enum.Parse(typeof(Type), args[Array.IndexOf(args, "-type") + 1]);
+            else
+            {
+                Console.Write("Output format (available values: mer, s): ");
+                outputType = (Type)Enum.Parse(typeof(Type), Console.ReadLine());
+            }
+
+            foreach (var path in Directory.EnumerateFiles(input, "*", SearchOption.AllDirectories))
+            {
+                var texName = Path.GetFileNameWithoutExtension(path);
+                var texTypeTxt = texName.Substring(texName.LastIndexOf("_") + 1);
+                var texType = (Type)Enum.Parse(typeof(Type), texTypeTxt);
+                texName = texName.Substring(0, texName.Length - texTypeTxt.Length - 1);
+                var outputName = $"{outputDir}{texName}_{Enum.GetName(typeof(Type), outputType)}.png";
+                using (Bitmap source = new Bitmap(path))
+                {
+                    var dest = new Bitmap(source.Width, source.Height);
+
+                    for (int x = 0; x < source.Width; x++)
+                        for (int y = 0; y < source.Height; y++)
+                            dest.SetPixel(x, y, Data.ToColor(Data.Parse(source.GetPixel(x, y), texType), outputType));
+                    dest.Save(outputName, ImageFormat.Png);
+                }
+                Console.WriteLine($"Saved at {outputName}");
+            }
+        }
+    }
+}
