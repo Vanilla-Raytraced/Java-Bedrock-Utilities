8a5a99367254dbd60e84b900c344e6566b82ebe8
EvanG <evan_g@orange.fr>
2021-02-24 15:16:37 +0100

    Improved logging

--- SpecularMerMaps/Program.cs
+++ SpecularMerMaps/Program.cs
@@ -10,10 +10,16 @@ namespace JavaBedrockUtilities
 {
     class Program
     {
+        public static LogLevel logLevel { get; private set; }
+        public static string inputPath { get; private set; }
+        public static Type inputType { get; private set; }
+        public static string outputPath { get; private set; }
+        public static Type outputType { get; private set; }
+
         static void Main(string[] args)
         {
             try { Prog(args); }
-            catch (Exception e) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(e); Console.ForegroundColor = ConsoleColor.White; }
+            catch (Exception e) { Log(e, LogLevel.Error); }
             finally
             {
                 Console.WriteLine("Press enter to close...");
@@ -22,44 +28,51 @@ namespace JavaBedrockUtilities
         }
         static void Prog(string[] args)
         {
-            string input;
-            if (args.Contains("-input")) input = args[Array.IndexOf(args, "-input") + 1];
+            object logLevelObj;
+            if (args.Contains("-log")) Enum.TryParse(typeof(LogLevel), args[Array.IndexOf(args, "-log") + 1], out logLevelObj);
             else
             {
-                Console.Write("Input dir (default: in): ");
-                input = Console.ReadLine();
+                Console.Write($"Level of logging (default: Info; available: {string.Join(", ", Enum.GetNames(typeof(LogLevel)))}): ");
+                Enum.TryParse(typeof(LogLevel), Console.ReadLine(), out logLevelObj);
             }
-            input = Path.GetFullPath(string.IsNullOrWhiteSpace(input) ? "in" : input).TrimEnd('\\', '/') + "/";
+            logLevel = logLevelObj == null ? LogLevel.Info : (LogLevel)logLevelObj;
+
+            if (args.Contains("-inputPath")) inputPath = args[Array.IndexOf(args, "-inputPath") + 1];
+            else
+            {
+                Console.Write("Input path (default: in): ");
+                inputPath = Console.ReadLine();
+            }
+            inputPath = Path.GetFullPath(string.IsNullOrWhiteSpace(inputPath) ? "in" : inputPath).TrimEnd('\\', '/') + "/";
 
             object inputTypeObj;
-            if (args.Contains("-type")) Enum.TryParse(typeof(Type), args[Array.IndexOf(args, "-type") + 1], out inputTypeObj);
+            if (args.Contains("-inputType")) Enum.TryParse(typeof(Type), args[Array.IndexOf(args, "-inputType") + 1], out inputTypeObj);
             else
             {
-                Console.Write("Input format (default: auto; available: auto, mer, s): ");
+                Console.Write($"Input format (default: auto; available: auto, {string.Join(", ", Enum.GetNames(typeof(Type)))}): ");
                 Enum.TryParse(typeof(Type), Console.ReadLine(), out inputTypeObj);
             }
-            var inputType = inputTypeObj == null ? (Type)(-1) : (Type)inputTypeObj;
+            inputType = inputTypeObj == null ? (Type)(-1) : (Type)inputTypeObj;
 
-            string outputDir;
-            if (args.Contains("-output")) outputDir = args[Array.IndexOf(args, "-output") + 1];
+            if (args.Contains("-outputPath")) outputPath = args[Array.IndexOf(args, "-outputPath") + 1];
             else
             {
-                Console.Write("Output dir (default: out): ");
-                outputDir = Console.ReadLine();
+                Console.Write("Output path (default: out): ");
+                outputPath = Console.ReadLine();
             }
-            outputDir = Path.GetFullPath(string.IsNullOrWhiteSpace(outputDir) ? "out" : outputDir).TrimEnd('\\', '/') + "/";
+            outputPath = Path.GetFullPath(string.IsNullOrWhiteSpace(outputPath) ? "out" : outputPath).TrimEnd('\\', '/') + "/";
 
             object outputTypeObj;
-            if (args.Contains("-type")) Enum.TryParse(typeof(Type), args[Array.IndexOf(args, "-type") + 1], out outputTypeObj);
+            if (args.Contains("-outputType")) Enum.TryParse(typeof(Type), args[Array.IndexOf(args, "-outputType") + 1], out outputTypeObj);
             else
             {
-                Console.Write("Output format (default: mer; available: mer, s): ");
+                Console.Write($"Output format (default: mer; available: {string.Join(", ", Enum.GetNames(typeof(Type)))}): ");
                 Enum.TryParse(typeof(Type), Console.ReadLine(), out outputTypeObj);
             }
-            var outputType = outputTypeObj == null ? Type.mer : (Type)outputTypeObj;
+            outputType = outputTypeObj == null ? Type.mer : (Type)outputTypeObj;
 
             var tasks = new List<Task>();
-            foreach (var path in Directory.EnumerateFiles(input, "*.png", SearchOption.AllDirectories))
+            foreach (var path in Directory.EnumerateFiles(inputPath, "*.png", SearchOption.AllDirectories))
                 tasks.Add(Task.Factory.StartNew(() => ProcessFile(path)));
             Task.WaitAll(tasks.ToArray());
 
@@ -67,18 +80,21 @@ namespace JavaBedrockUtilities
             {
                 try
                 {
-                    var texPath = Path.GetRelativePath(input, Path.GetDirectoryName(path));
+                    Log($"Processing {path}", LogLevel.Debug);
+                    var texPath = Path.GetRelativePath(inputPath, Path.GetDirectoryName(path));
                     var texName = Path.GetFileNameWithoutExtension(path);
                     var texType = inputType;
                     if (inputType == (Type)(-1))
                     {
                         var texTypeTxt = texName.Substring(texName.LastIndexOf("_") + 1);
                         if (Enum.TryParse(typeof(Type), texTypeTxt, out var texTypeObj)) texType = (Type)texTypeObj;
-                        else throw new NotSupportedException("Format " + texTypeTxt + " isn't supported");
+                        else throw new NotSupportedException($"Format \"{texTypeTxt}\" isn't supported: {path}");
                         texName = texName.Substring(0, texName.Length - texTypeTxt.Length - 1);
                     }
-                    var outputName = $"{outputDir}{texPath}/{texName}_{Enum.GetName(typeof(Type), outputType)}.png";
-                    if (!Directory.Exists(outputDir + texPath)) Directory.CreateDirectory(outputDir + texPath);
+                    var outputName = $"{outputPath}{texPath}/{texName}_{Enum.GetName(typeof(Type), outputType)}.png";
+                    if (!Directory.Exists(outputPath + texPath)) Directory.CreateDirectory(outputPath + texPath);
+
+                    Log($"Converting {Path.GetRelativePath(inputPath, path)} from {texType} to {outputType}", LogLevel.Debug);
                     using (Bitmap source = new Bitmap(path))
                     {
                         var dest = new Bitmap(source.Width, source.Height);
@@ -88,10 +104,31 @@ namespace JavaBedrockUtilities
                                 dest.SetPixel(x, y, Data.ToColor(Data.Parse(source.GetPixel(x, y), texType), outputType));
                         dest.Save(outputName, ImageFormat.Png);
                     }
-                    Console.WriteLine($"Saved at {outputName}");
+
+                    Log($"Saved at {Path.GetFullPath(outputName)}", LogLevel.Success);
                 }
-                catch (Exception e) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(e); Console.ForegroundColor = ConsoleColor.White; }
+                catch (NotSupportedException e) { Log(e.Message, LogLevel.Warning); }
+                catch (Exception e) { Log(e, LogLevel.Error); }
             }
         }
+
+        public enum LogLevel { Debug, Info, Success, Warning, Error }
+        static void Log(Exception e, LogLevel level = LogLevel.Error) => Log(e.ToString(), level);
+        static void Log(string message, LogLevel level = LogLevel.Info)
+        {
+            if (level < logLevel) return;
+            Console.ForegroundColor = LogColor(level);
+            Console.WriteLine(message);
+            Console.ForegroundColor = LogColor(LogLevel.Info);
+        }
+        static ConsoleColor LogColor(LogLevel level) => level switch
+        {
+            LogLevel.Debug => ConsoleColor.DarkGray,
+            LogLevel.Info => ConsoleColor.Gray,
+            LogLevel.Success => ConsoleColor.Green,
+            LogLevel.Warning => ConsoleColor.Yellow,
+            LogLevel.Error => ConsoleColor.Red,
+            _ => ConsoleColor.Gray
+        };
     }
 }
